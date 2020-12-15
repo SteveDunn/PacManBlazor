98328bf5d8a17b5877f5085d645486c0c0b62ee4
Steve Dunn <steve@dunnhq.com>
2020-12-15 20:40:22 +0000

    Revert top level statements as it crashes Blazor: https://github.com/dotnet/aspnetcore/issues/28677

--- src/PacMan/Program.cs
+++ src/PacMan/Program.cs
@@ -14,79 +14,89 @@ using PacMan.GameComponents.Ghosts;
 using PacMan.GameComponents.Requests;
 using PacMan;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("app");
-
-IServiceCollection services = builder.Services;
-
-services.AddSingleton<IGame, Game>();
-services.AddSingleton<IGameStorage, GameStorage>();
-services.AddSingleton<IHumanInterfaceParser, HumanInterfaceParser>();
-services.AddSingleton<ISoundLoader, SoundLoader>();
-services.AddSingleton<IGameSoundPlayer, GameSoundPlayer>();
-
-services.AddSingleton<IAct, AttractAct>();
-services.AddSingleton<IAct, GameAct>();
-
-services.AddSingleton<IAct, BigPacChaseAct>();
-services.AddSingleton<IAct, GameOverAct>();
-services.AddSingleton<IAct, GhostTearAct>();
-services.AddSingleton<IAct, LevelFinishedAct>();
-services.AddSingleton<IAct, NullAct>();
-services.AddSingleton<IAct, PacManDyingAct>();
-services.AddSingleton<IAct, PlayerIntroAct>();
-services.AddSingleton<IAct, DemoPlayerIntroAct>();
-services.AddSingleton<IAct, StartButtonAct>();
-services.AddSingleton<IAct, TornGhostChaseAct>();
-
-services.AddSingleton<IAct, PlayerGameOverAct>();
-
-services.AddSingleton<IActs, Acts>();
-
-services.AddSingleton<IGameStats, GameStats>();
-
-services.AddSingleton<IHaveTheMazeCanvases, MazeCanvases>();
-
-services.AddSingleton<IGhost, Blinky>();
-services.AddSingleton<IGhost, Pinky>();
-services.AddSingleton<IGhost, Inky>();
-services.AddSingleton<IGhost, Clyde>();
-
-services.AddSingleton<IPacMan, PacMan.GameComponents.PacMan>();
-
-services.AddSingleton<ICoinBox, CoinBox>();
-
-services.AddSingleton<IFruit, Fruit>();
-
-services.AddSingleton<IGhostCollection, GhostCollection>();
-
-services.AddSingleton<IStatusPanel, StatusPanel>();
-services.AddSingleton<IScorePanel, ScorePanel>();
-
-services.AddSingleton<IMaze, Maze>();
-
-services.AddLogging();
-
-services.Add(new(
-    typeof(IExceptionNotificationService),
-    typeof(ExceptionNotificationService),
-    ServiceLifetime.Singleton));
-
-Assembly thisAssembly = Assembly.GetExecutingAssembly();
-
-Assembly componentsAssembly = typeof(ClassThatLivesInGameComponentsActsAsAMarkerForThisAssemblyForReflection).Assembly;
-
-services.AddMediatR(
-    c =>
-        c.AsSingleton(),
-    thisAssembly,
-    componentsAssembly);
-
-services.AddSingleton(new HttpClient
+namespace Whatever
 {
-    BaseAddress = new(builder.HostEnvironment.BaseAddress)
-});
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
 
-var host = builder.Build();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
 
-await host.RunAsync();
+            IServiceCollection services = builder.Services;
+
+            services.AddSingleton<IGame, Game>();
+            services.AddSingleton<IGameStorage, GameStorage>();
+            services.AddSingleton<IHumanInterfaceParser, HumanInterfaceParser>();
+            services.AddSingleton<ISoundLoader, SoundLoader>();
+            services.AddSingleton<IGameSoundPlayer, GameSoundPlayer>();
+
+            services.AddSingleton<IAct, AttractAct>();
+            services.AddSingleton<IAct, GameAct>();
+
+            services.AddSingleton<IAct, BigPacChaseAct>();
+            services.AddSingleton<IAct, GameOverAct>();
+            services.AddSingleton<IAct, GhostTearAct>();
+            services.AddSingleton<IAct, LevelFinishedAct>();
+            services.AddSingleton<IAct, NullAct>();
+            services.AddSingleton<IAct, PacManDyingAct>();
+            services.AddSingleton<IAct, PlayerIntroAct>();
+            services.AddSingleton<IAct, DemoPlayerIntroAct>();
+            services.AddSingleton<IAct, StartButtonAct>();
+            services.AddSingleton<IAct, TornGhostChaseAct>();
+
+            services.AddSingleton<IAct, PlayerGameOverAct>();
+
+            services.AddSingleton<IActs, Acts>();
+
+            services.AddSingleton<IGameStats, GameStats>();
+
+            services.AddSingleton<IHaveTheMazeCanvases, MazeCanvases>();
+
+            services.AddSingleton<IGhost, Blinky>();
+            services.AddSingleton<IGhost, Pinky>();
+            services.AddSingleton<IGhost, Inky>();
+            services.AddSingleton<IGhost, Clyde>();
+
+            services.AddSingleton<IPacMan, PacMan.GameComponents.PacMan>();
+
+            services.AddSingleton<ICoinBox, CoinBox>();
+
+            services.AddSingleton<IFruit, Fruit>();
+
+            services.AddSingleton<IGhostCollection, GhostCollection>();
+
+            services.AddSingleton<IStatusPanel, StatusPanel>();
+            services.AddSingleton<IScorePanel, ScorePanel>();
+
+            services.AddSingleton<IMaze, Maze>();
+
+            services.AddLogging();
+
+            services.Add(
+                new(
+                    typeof(IExceptionNotificationService),
+                    typeof(ExceptionNotificationService),
+                    ServiceLifetime.Singleton));
+
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+
+            Assembly componentsAssembly =
+                typeof(ClassThatLivesInGameComponentsActsAsAMarkerForThisAssemblyForReflection).Assembly;
+
+            services.AddMediatR(
+                c =>
+                    c.AsSingleton(),
+                thisAssembly,
+                componentsAssembly);
+
+            services.AddSingleton(new HttpClient {BaseAddress = new(builder.HostEnvironment.BaseAddress)});
+
+            var host = builder.Build();
+
+            await host.RunAsync();
+
+        }
+    }
+}
