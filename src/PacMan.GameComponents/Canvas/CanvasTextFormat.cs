b7df2f874fb9ceb33f0865557cf4819880975781
Steve Dunn <steve@dunnhq.com>
2020-12-15 20:41:30 +0000

    Fixed heap allocation warning

--- src/PacMan.GameComponents/Canvas/CanvasTextFormat.cs
+++ src/PacMan.GameComponents/Canvas/CanvasTextFormat.cs
@@ -2,6 +2,7 @@
 {
 public record CanvasTextFormat(string FontFamily, int FontSize)
 {
+    // ReSharper disable once HeapView.BoxingAllocation
     public readonly string FormattedString =  $"{FontSize}px {FontFamily}";
 }
 }
